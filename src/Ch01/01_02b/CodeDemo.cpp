59aa4ac66ef9002a124bbd1f3092215d4361a8b3
Teoball Knutsen <teoballen@gmail.com>
2024-04-25 11:26:10 +0000

    ja

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -2,10 +2,15 @@
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
 #include <iostream>
+#include <string>
 
 int main () {
-    std::cout << "Hei!" << std::endl;
-
+    std::cout << "Hei! Hva heter du?" << std::endl;
+    std::string str;
+    std::cin >> str;
+   
+    std::cout << std::endl << std::endl;
+    std::cout << "Hei " << str << "!" << std::endl;
     std::cout << std::endl << std::endl;
     return(0);
 }
