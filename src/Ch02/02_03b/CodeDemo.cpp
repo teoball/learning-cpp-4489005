4fd1bf88d09a72f7c639948bf4ae5f712182c74d
Teoball Knutsen <teoballen@gmail.com>
2024-04-25 11:38:45 +0000

    ny

--- src/Ch02/02_03b/CodeDemo.cpp
+++ src/Ch02/02_03b/CodeDemo.cpp
@@ -4,8 +4,18 @@
 
 #include <iostream>
 
+int a, b = 5;
+
 int main(){
-    std::cout << "Hi There!" << std::endl;
+    bool my_flag;
+    a = 7;
+    my_flag = true;
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+    std::cout << "flag = " << my_flag << std::endl;
+    std::cout << "a + b = " << a+b << std::endl;
+    std::cout << "a-b = " << a-b << std::endl;
+    std::cout << "a + flag = " << a+my_flag << std::endl;
     
     std::cout << std::endl << std::endl;
     return (0);
