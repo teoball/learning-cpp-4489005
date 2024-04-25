9e920a1e6d20b8e0ea8af8ace256aea85cb34d4d
Teoball Knutsen <teoballen@gmail.com>
2024-04-25 12:26:32 +0000

    j

--- src/Ch02/02_05b/CodeDemo.cpp
+++ src/Ch02/02_05b/CodeDemo.cpp
@@ -6,7 +6,22 @@
 #include <typeinfo>
 
 int main(){
-    
+    auto a = 8;
+    auto b = 12345678901;
+    auto c = 3.14f;
+    auto d = 3.14;
+    auto e = true;
+    auto f = 'd';
+    auto g = "Torbein er kool!";
+
+    std::cout << "Denne typen er " << typeid(a).name() << std::endl;
+    std::cout << "Denne typen er " << typeid(b).name() << std::endl;
+    std::cout << "Denne typen er " << typeid(c).name() << std::endl;
+    std::cout << "Denne typen er " << typeid(d).name() << std::endl;
+    std::cout << "Denne typen er " << typeid(e).name() << std::endl;
+    std::cout << "Denne typen er " << typeid(f).name() << std::endl;
+    std::cout << "Denne typen er " << typeid(g).name() << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
