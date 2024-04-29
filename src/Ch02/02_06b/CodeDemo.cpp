5d4fd37da261fecf59e0c0e63d71e30600bbd959
Teoball Knutsen <teoballen@gmail.com>
2024-04-29 08:44:09 +0000

    asd

--- src/Ch02/02_06b/CodeDemo.cpp
+++ src/Ch02/02_06b/CodeDemo.cpp
@@ -3,9 +3,20 @@
 // Preprocessor directives, by Eduardo Corpeño 
 
 #include <iostream>
+#include <string>
+#include <cstdint>
 
+#define CAPACITY 5000
+#define DEBUG
 int main(){
-    
+    int32_t large = CAPACITY;
+    uint8_t small =37;
+#ifdef DEBUG
+std::cout << "Regner dette ut " << std::endl;
+#endif
+    large += small; //large = large + small;
+    std::cout << "Det store tallet er " << large << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
